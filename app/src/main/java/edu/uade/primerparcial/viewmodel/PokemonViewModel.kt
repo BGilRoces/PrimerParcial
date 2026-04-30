d6083de3127b1dd9f3a52040105e55de204041b0
BGilRoces <bgilroces@uade.edu.ar>
2026-04-30 18:53:31 -0300

    GilRoces Initial Commit

---
+++ app/src/main/java/edu/uade/primerparcial/viewmodel/PokemonViewModel.kt
@@ -0,0 +1,23 @@
+package edu.uade.primerparcial.viewmodel
+
+import androidx.lifecycle.ViewModel
+import edu.uade.primerparcial.data.model.Pokemon
+import edu.uade.primerparcial.data.repository.PokemonRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class PokemonViewModel(
+    private val repository: PokemonRepository
+) : ViewModel() {
+    private val _pokemons = MutableStateFlow<List<Pokemon>>(emptyList())
+    val pokemons: StateFlow<List<Pokemon>> = _pokemons.asStateFlow()
+
+    init {
+        loadPokemons()
+    }
+
+    private fun loadPokemons() {
+        _pokemons.value = repository.getPokemons()
+    }
+}
